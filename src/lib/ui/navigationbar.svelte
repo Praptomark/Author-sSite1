e43e9bdb75acca97d3f387666bcfe77237be661a
Prapto Mark <51514572+Praptomark@users.noreply.github.com>
2023-11-12 02:58:06 +0600

    Fixed the position of the nav bar

--- src/lib/ui/navigationbar.svelte
+++ src/lib/ui/navigationbar.svelte
@@ -2,7 +2,7 @@
     export let name: string
 </script>
 
-<nav class="flex gap-2 justify-around items-center p-3 bg-slate-400">
+<nav class="flex gap-2 justify-around items-center p-3 bg-slate-400 fixed w-full z-50">
     <h1 class="text-4xl font-satisfy cursor-default text-white">{name}</h1>
     <ul class="flex gap-4 font-poppins">
         <li><a class="text-white cursor-default hover:underline hover:underline-offset-4 active:text-slate-300 active:transition active:ease-in active:duration-75" href="/">HOME</a></li>
